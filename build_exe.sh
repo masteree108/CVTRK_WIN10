masteree108/CVTRK_UBU18

(NTUT)(EXE) build an vott_tracker.exe for vott to track object
1.how to build an exe file,please as follows
  $ ./build_exe.sh
  execute it will be created vott_tracker.exe file and copied to below path:
  ../../VoTT_NTUT/UBU18/NTUT/exe/vott_tracker.exe
  (please note this path is following where downloaded project by repo command)

2. modified log.py(save_path) file for vott. when user using label tool
   will be executed vott_tracker.exe and saved log.txt at save_path

3. doc_and_testcode/run_hello_exe just a test file

4. the details please refer to the below doc:
https://hackmd.io/@NTUTVOTT/HyrJJtDvP

Dependence:
 CVTRK/UBU18
 VoTT_NTUT/UBU18 commit: "(NTUT)(SERVICE) added a service to detect and execute vott_tracker.exe automatically"

build_exe.sh
@@ -1,0 +1,12 @@
+# note 1: if your os is windows10 please changing build_exe.sh to build_exe.bat
+# note 2: this buiild_exe.sh that needs to setup module please as follows
+# pip install pyinstaller
+# note 3: chmod +x build_exe.sh
+
+pyinstaller -F ./vott_tracker.py
+cp ./dist/vott_tracker ../../VoTT_NTUT/UBU18/NTUT/exe/vott_tracker.exe
+cp ./dist/vott_tracker ./vott_tracker.exe
+rm -rf dist
+rm -rf __pycache__
+rm -rf build
+rm *.spec

masteree108/CVTRK_WIN10

 CVTRK/WIN10
 VoTT_NTUT/WIN10 commit: "(NTUT)(SERVICE) added a service to detect and execute vott_tracker.exe automatically"

build_exe.sh
@@ -1,0 +1,12 @@
+# note 1: if your os is windows10 please changing build_exe.sh to build_exe.bat
+# note 2: this buiild_exe.sh that needs to setup module please as follows
+# pip install pyinstaller
+# note 3: chmod +x build_exe.sh
+
+pyinstaller -F ./vott_tracker.py
+cp ./dist/vott_tracker ../../VoTT_NTUT/WIN10/NTUT/exe/vott_tracker.exe
+cp ./dist/vott_tracker ./vott_tracker.exe
+rm -rf dist
+rm -rf __pycache__
+rm -rf build
+rm *.spec