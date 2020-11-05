b5e7f6296a60148ade944aecd063dc83e60371ca
t108368527 <you2623t@gmail.com>
2020-11-06 00:50:13 +0800

    (NTUT)(RELEASE) NTUT_VoTT_2020_11_05_v0.0.2
    about details please referring release_note
    
    Dependence:
      CVTRK\WIN10
      VoTT_NTUT\WIN10

--- build_exe.sh
+++ build_exe.sh
@@ -1,11 +1,11 @@
-# note 1: if your os is windows10 please changing build_exe.sh to build_exe.bat
+# note 1: if your os is windows10 please changing build_exe.sh to build_exe.bat(it dosen't need if it runs at the cygwin terminal)
 # note 2: this buiild_exe.sh that needs to setup module please as follows
 # pip install pyinstaller
 # note 3: chmod +x build_exe.sh
 
 pyinstaller -F ./vott_tracker.py
-cp ./dist/vott_tracker ../../VoTT_NTUT/WIN10/NTUT/exe/vott_tracker.exe
-cp ./dist/vott_tracker ./vott_tracker.exe
+cp -f ./dist/vott_tracker ../../VoTT_NTUT/WIN10/NTUT/exe/vott_tracker.exe
+cp -f ./dist/vott_tracker ./vott_tracker.exe
 rm -rf dist
 rm -rf __pycache__
 rm -rf build
